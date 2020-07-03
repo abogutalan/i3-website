fe5d8285ebb20d4da21dd935c547fc1c33f96244
Faizah Afreen <faizah.afreen@ontariotechu.net>
2020-07-03 10:53:04 -0400

    ais

---
+++ src/data/ais.js
@@ -0,0 +1,38 @@
+export const ais = [
+    {
+        d1: 'Our super intensive initiative that is designed to change the Islamic landscape in Canada!',
+        d2: `The YD Program has been a great success for the last few years. 
+             We have had 13 classes operating, and we think it is the right time to introduce our community to a new program....`,
+        d3: `This program is super intensive and very comprehensive and covers a structure of
+             material similar to that of Islamic universities.
+             This is for those who want to take their knowledge to the next level and cause
+             change in their own lives. We are asking all the highly committed students to
+             sign up.`,
+        d4: `You can expect to cover a wide range of topics like Tafsir, classical Fiqh
+            texts,
+            Comparative Fiqh, Quranic Sciences, Hadith Sciences, Fiqh of Da'wah, Aqeedah,
+            Tazkiya and Tasawwuf, and much more.`,
+        d5: `An equivalent program costs over $20,000 CAD. However, we are offering this
+             program for $100 per term (Excluding a registration fee), for which you will
+             have
+             online access to, from the comfort of your own home.`,
+        d6: `Here is a more detailed list of the objectives and content of this program.`,
+        d7: `Finish reading 2 full tafsirs of fhe Quran.`,
+        d8: `Study a main classical fiqh text and its one of its explanations for all 4 schools
+             of thoughts.`,
+        d9: `Study a comparative fiqh module.`,
+        d10: `Study at least 2 classical Usul al-Fiqh books and their explanations.`,
+        d11: `Study in depth the sciences of the Quran and the principles that guide a Muffasir
+              and the different schools of thought within the exegesis of the Quran.`,
+        d12: `Study 2 main and comprehensive books on the sciences of Hadtih.`,
+        d13: `Read fully 2 books of Hadith from front to back with 2 of their respective classical
+              explanations.`,
+        d14: `Read the belief systems of all main Islamic schools of thoughts and learn about the
+              different sects of the past and modern times.`,
+        d15: `Study 3 classical and contemporary books on Fiqh of Dawah.`,
+        d16: `Study the first 3 periods of Islamic History: the Prophets and their Nations, the
+              Seerah of Prophet Muhammad (pbuh) and the history of the Khulafa Rashudoon.`,
+        d17: `Study a comprehensive book on Tazkiyyah and Tasawwuf from a main classical book.`,
+        d18: `Study logic from the Arabic early Muslim scholars perspective.`
+    }
+]
