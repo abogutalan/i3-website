e4d0f07a4d6eef88c60a7004aa7adca639d31155
abogutalan <abogutalan35@gmail.com>
2020-07-08 10:12:28 -0400

    cleaned

--- src/pages/programs/FCC.js
+++ src/pages/programs/FCC.js
@@ -1,14 +1,4 @@
 import React from 'react';
-<<<<<<< HEAD
-const CounselorsPage = ({data}) => (
-    <div>
-      <h1>CounselorsPage Page</h1>
-     
-    </div>
-      
-  )
-
-=======
 
 const CounselorsPage = ({data}) => (
     <div>
@@ -17,6 +7,5 @@ const CounselorsPage = ({data}) => (
     </div>
       
   )
->>>>>>> f5669e576ed35c56d7c5da3fc7783758c09f4ac9
 
 export default CounselorsPage;
