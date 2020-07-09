2bb349987b12601f5fd4bdceb963747963c5839b
abogutalan <abogutalan35@gmail.com>
2020-07-09 10:12:47 -0400

    fixed volunteer path

--- src/data/volunteers.js
+++ src/data/volunteers.js
@@ -1,49 +1,49 @@
-// export const volunteers = [
-//     {
-//         jobTitle: "Creative Producer",
-//         jobTeam: "Research Center",
-//         description: `The Research centers purpose is to the be the catalyst
-//         for clarifying Islamic thought and innovation. It is attended by
-//         prominent researchers who have contributed to different aspect of
-//         societal issues. The creative producer is the most vital asset in
-//         handling all aspects of video production towards media distribution channels at scale.`,
-//         duties: [`Oversee all activities and delivery of services, ensuring high quality and adherence to schedules/timelines and KPI’s`,
-//         `Manage performance, set priorities, and provide timely feedback and coaching`,
-//         `Ensure board meetings are focused and disciplined`,
-//         `Help the team leads set milestones for themselves and their teams and keep them accountable`,
-//         `Ensure i3’s values are integrated in the process and in the organization`],
-//         qualifications: [`Previous team leadership and project management experience is a MUST.`, 
-//         `This is a role that will be working side by side with the board.`,
-//         `Minimum 3-5 years experience leading teams`,
-//         `Experience coordinating or managing Projects/Programs`,
-//         `Experience managing relationships with clients/stakeholders`,
-//         `Highly disciplined`,
-//         `Pragmatic`,
-//         `Leadership`,
-//         `100% positive attitude`,
-//         `If interested in applying, please contact volunteer@i3institute.ca`]
-//     },
-//     {
-//         jobTitle: "Program Manager",
-//         jobTeam: "Board",
-//         description: `The PM acts as the consultant for the board of the institute.
-//         They ensure all tasks are on track and all projects are executed until completion.`,
-//         duties: [`Oversee all activities and delivery of services, ensuring high quality and adherence to
-//         schedules/timelines and KPI’s`,
-//         `Manage performance, set priorities, and provide timely feedback and coaching`,
-//         `Ensure board meetings are focused and disciplined`,
-//         `Help the team leads set milestones for themselves and their teams and keep them accountable`,
-//         `Ensure i3’s values are integrated in the process and in the organization`],
-//         qualifications: [`Previous team leadership and project management experience is a MUST.\n`, 
-//         `This is a role that will be working side by side with the board.`,
-//         `Minimum 3-5 years’ experience leading teams`,
-//         `Experience coordinating or managing Projects/Programs`,
-//         `Experience managing relationships with clients/stakeholders`,
-//         `Highly disciplined`,
-//         `Pragmatic`,
-//         `Leadership`,
-//         `100% positive attitude`,
-//         `If interested in applying, please contact volunteer@i3institute.ca`],
-//         //email : <a href = "volunteer@i3institute.ca"> volunteer@i3institute.ca </a>,
-//     }
-// ]
+export const volunteers = [
+    {
+        jobTitle: "Creative Producer",
+        jobTeam: "Research Center",
+        description: `The Research centers purpose is to the be the catalyst
+        for clarifying Islamic thought and innovation. It is attended by
+        prominent researchers who have contributed to different aspect of
+        societal issues. The creative producer is the most vital asset in
+        handling all aspects of video production towards media distribution channels at scale.`,
+        duties: [`Oversee all activities and delivery of services, ensuring high quality and adherence to schedules/timelines and KPI’s`,
+        `Manage performance, set priorities, and provide timely feedback and coaching`,
+        `Ensure board meetings are focused and disciplined`,
+        `Help the team leads set milestones for themselves and their teams and keep them accountable`,
+        `Ensure i3’s values are integrated in the process and in the organization`],
+        qualifications: [`Previous team leadership and project management experience is a MUST.`, 
+        `This is a role that will be working side by side with the board.`,
+        `Minimum 3-5 years experience leading teams`,
+        `Experience coordinating or managing Projects/Programs`,
+        `Experience managing relationships with clients/stakeholders`,
+        `Highly disciplined`,
+        `Pragmatic`,
+        `Leadership`,
+        `100% positive attitude`,
+        `If interested in applying, please contact volunteer@i3institute.ca`]
+    },
+    {
+        jobTitle: "Program Manager",
+        jobTeam: "Board",
+        description: `The PM acts as the consultant for the board of the institute.
+        They ensure all tasks are on track and all projects are executed until completion.`,
+        duties: [`Oversee all activities and delivery of services, ensuring high quality and adherence to
+        schedules/timelines and KPI’s`,
+        `Manage performance, set priorities, and provide timely feedback and coaching`,
+        `Ensure board meetings are focused and disciplined`,
+        `Help the team leads set milestones for themselves and their teams and keep them accountable`,
+        `Ensure i3’s values are integrated in the process and in the organization`],
+        qualifications: [`Previous team leadership and project management experience is a MUST.\n`, 
+        `This is a role that will be working side by side with the board.`,
+        `Minimum 3-5 years’ experience leading teams`,
+        `Experience coordinating or managing Projects/Programs`,
+        `Experience managing relationships with clients/stakeholders`,
+        `Highly disciplined`,
+        `Pragmatic`,
+        `Leadership`,
+        `100% positive attitude`,
+        `If interested in applying, please contact volunteer@i3institute.ca`],
+        //email : <a href = "volunteer@i3institute.ca"> volunteer@i3institute.ca </a>,
+    }
+]
