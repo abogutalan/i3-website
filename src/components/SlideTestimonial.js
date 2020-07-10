beef37a5b120875d4ff0ba0b9f5931d9d79e6b85
Faizah Afreen <faizah.afreen@ontariotechu.net>
2020-07-10 12:11:56 -0400

    added a few pages

---
+++ src/components/SlideTestimonial.js
@@ -0,0 +1,29 @@
+// import React from 'react'
+
+// const SlideTestimonial = ({imgURL, quote, author, position}) => (
+//     <>
+//     <div class="carousel-item">
+//         <div class="card card-testimonial card-plain">
+//             <div class="card-avatar">
+//                 <img class="img" src={imgURL} />
+//             </div>
+//             <div class="card-body">
+//                 <h5 class="card-description">
+//                     "{quote}"
+//                 </h5>
+//                 <h4 class="card-title">{author}</h4>
+//                 <h6 class="card-category text-muted">{position}</h6>
+//                     <div class="footer">
+//                         <i class="material-icons text-warning">star</i>
+//                         <i class="material-icons text-warning">star</i>
+//                         <i class="material-icons text-warning">star</i>
+//                         <i class="material-icons text-warning">star</i>
+//                         <i class="material-icons text-warning">star</i>
+//                     </div>
+//                 </div>
+//             </div>
+//         </div>
+//     </>
+// )
+
+// export default SlideTestimonial
---
+++ src/components/Testimonial.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const Testimonial = ({quote, author, position, imgURL}) => (
+    <>
+        <div class="col-md-4">
+            <div class="card card-testimonial">
+                <div class="icon">
+                    <i class="material-icons">format_quote</i>
+                </div>
+                <div class="card-body ">
+                    <h5 class="card-description"> "{quote}" </h5>
+                </div>
+                <div class="card-footer ">
+                    <h4 class="card-title">{author}</h4>
+                    <h6 class="card-category">{position}</h6>
+                    <div class="card-avatar">
+                        <img class="img" src={imgURL} />
+                    </div>
+                </div>
+            </div>
+        </div>
+    </>
+)
+
+// export default Testimonial
