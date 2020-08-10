4464dd4bf1ac114f02a817452f11ef3be6128015
Burhan Ogutalan <abogutalan@Burhans-MacBook-Pro.local>
2020-08-10 12:02:01 -0400

    history corrected

--- src/pages/about/history.js
+++ src/pages/about/history.js
@@ -2,159 +2,138 @@ import React from 'react'
 import Layout from '../../components/layout'
 
 const History = () => (
-    <>
-        <Layout>
-            <div className = "about-us sidebar-collapse">
-                <div className = "page-header header-filter header-small" data-parallax = "true"
-                    style = {{ backgroundImage: `url('/myAssets/img/AppreciationDinner.jpg')` }} >
-                    <div className = "container">
-                        <div className = "about-us sidebar-collapse">
-                            <div className = "page-header header-filter header-small" data-parallax = "true"
-                                style = {{ backgroundImage: `url('/myAssets/img/AppreciationDinner.jpg')` }} >
-                                <div className = "container">
-                                    <div className = "row">
-                                        <div className = "col-md-8 ml-auto mr-auto text-center">
-                                            <h1 className = "title"> Objective </h1>
-                                            <h4> Find out how i3 started and what it has to offer you! </h4>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className = "main main-raised">
-                                <div className = "container">
-                                    <div className = "about-description text-center">
-                                        <div className = "row">
-                                            <div className = "col-md-8 ml-auto mr-auto text-center">
-                                                <h1 className = "title"> Objective </h1>
-                                                <h4> Find out how i3 started and what it has to offer you! </h4>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div className = "main main-raised">
-                                    <div className = "container">
-                                        <div className = "about-description text-center">
-                                            <div className = "row">
-                                                <div className = "col-md-8 ml-auto mr-auto">
-                                                    <h2 className = "title"> How It All Began </h2>
-                                                    <h5 className = "description">
-                                                        i3 was built in April 2014 with the mission of creating a community of
-                                                        young, aspiring and visionary leaders who are principled, skilled, empowered and aware of
-                                                        current issues. We aim to build an aware, confident and active group of people that have a
-                                                        positive impact in the Canadian Civil Society.
-                                </h5>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <div className = "about-services features-2">
-                                            <div className = "section">
-                                                <div className = "row">
-                                                    <div className = "col-md-8 ml-auto mr-auto text-center">
-                                                        <h2 className = "title"> Focus & Objective </h2>
-                                                    </div>
-                                                </div>
-                                                <div className = "row">
-                                                    <div className = "col-md-4">
-                                                        <div className = "info info-horizontal">
-                                                            <div className = "icon icon-rose">
-                                                                <i className = "material-icons"> build </i>
-                                                            </div>
-                                                            <div className = "description">
-                                                                <h4 className = "info-title"> 1. Leadership Building </h4>
-                                                                <p>
-                                                                    i3 has already established itself as a key grassroots organization that has
-                                                                    offered leadership programs, conferences, workshops, social retreats, youth
-                                                                    activities, and seminars.
-                                            </p>
-                                                            </div>
-                                                        </div>
-                                                    </div>
-                                                    <div className = "col-md-4">
-                                                        <div className = "info info-horizontal">
-                                                            <div className = "icon icon-rose">
-                                                                <i className = "material-icons"> library_books </i>
-                                                            </div>
-                                                            <div className = "description">
-                                                                <h4 className = "info-title"> 2. Instilling Knowledge </h4>
-                                                                <p>
-                                                                    i3 provides learning and empowerment for the Muslim Youth
-                                                                    for them to be confident, productive and aware individuals who can address
-                                                                    racism and Islamophobia in society.
-                                            </p>
-                                                            </div>
-                                                        </div>
-                                                    </div>
-                                                    <div className = "col-md-4">
-                                                        <div className = "info info-horizontal">
-                                                            <div className = "icon icon-rose">
-                                                                <i className = "material-icons"> personal_video </i>
-                                                            </div>
-                                                            <div className = "description">
-                                                                <h4 className = "info-title"> 3. Wholistic Approach </h4>
-                                                                <p>
-                                                                    i3 will maintain a center wherein individuals’ spiritual, social, cultural and
-                                                                    religious identity is built.
-                                            </p>
-                                                            </div>
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                            <div className = "features-3">
-                                                <div className =" row">
-                                                    <div className = "col-md-6">
-                                                        <br />
-                                                        <br />
-                                                        <div>
-                                                            <img style = {{ width: '90%' }} src = "/myAssets/img/knowledge.png" alt = "" />
-                                                        </div>
-                                                    </div>
-                                                    <div className = "col-md-6">
-
-                                                        <h2 className = "title"> Our Track Record </h2>
-                                                        <div className = "info info-horizontal">
-                                                            <div className = "icon icon-primary">
-                                                                <i className = "material-icons"> arrow_forward </i>
-                                                            </div>
-                                                            <div className = "description">
-                                                                <h4 className = "info-title"> Our Impact </h4>
-                                                                <p>
-                                                                    As a grassroots organization, i3 has provided educational programs, social
-                                                                    events, workshops and seminars that have had an impact on hundreds of Muslim
-                                                                    youth over the past four years. Many of these events included vital modules that
-                                                                    addressed various forms of racism faced by other minority groups that we’ve
-                                                                    worked with; our attendees and students found them incredibly beneficial.
-                                            </p>
-                                                            </div>
-                                                        </div>
-                                                        <div className = "info info-horizontal">
-                                                            <div className = "icon icon-primary">
-                                                                <i className = "material-icons"> money_off </i>
-                                                            </div>
-                                                            <div className = "description">
-                                                                <h4 className = "info-title"> An unbeatable offer! </h4>
-                                                                <p>
-                                                                    Our educational style, even though deep and intensive given the
-                                                                    nature of the material taught, can easily be adapted into the busy
-                                                                    lifestyle of Canadian youth. More importantly,
-                                                                    all our programming is free of charge to ease the process of
-                                                                    seeking Islamic knowledge.
-                                            </p>
-                                                            </div>
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+    <Layout>
+        <div className="about-us sidebar-collapse">
+        
+        <div id="navbarDiv">
+        </div>
+        <div className="page-header header-filter header-small" data-parallax="true" style={{backgroundImage: 'url("/myAssets/img/AppreciationDinner.jpg")'}}>
+          <div className="container">
+            <div className="row">
+              <div className="col-md-8 ml-auto mr-auto text-center">
+                <h1 className="title">Objective</h1>
+                <h4>Find out how i3 started and what it has to offer you!</h4>
+              </div>
             </div>
-        </Layout>
-    </>
+          </div>
+        </div>
+        <div className="main main-raised">
+          <div className="container">
+            <div className="about-description text-center">
+              <div className="row">
+                <div className="col-md-8 ml-auto mr-auto">
+                  <h2 className="title">How It All Began</h2>
+                  <h5 className="description">i3 was built in April 2014 with the mission of creating a community of
+                    young, aspiring and visionary leaders who are principled, skilled, empowered and aware of
+                    current issues. We aim to build an aware, confident and active group of people that have a
+                    positive impact in the Canadian Civil Society.
+                  </h5>
+                </div>
+              </div>
+            </div>
+            <div className="about-services features-2">
+              <div className="section">
+                <div className="row">
+                  <div className="col-md-8 ml-auto mr-auto text-center">
+                    <h2 className="title">Focus &amp; Objective</h2>
+                  </div>
+                </div>
+                <div className="row">
+                  <div className="col-md-4">
+                    <div className="info info-horizontal">
+                      <div className="icon icon-rose">
+                        <i className="material-icons">build</i>
+                      </div>
+                      <div className="description">
+                        <h4 className="info-title">1. Leadership Building</h4>
+                        <p>
+                          i3 has already established itself as a key grassroots organization that has
+                          offered leadership programs, conferences, workshops, social retreats, youth
+                          activities, and seminars.
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="col-md-4">
+                    <div className="info info-horizontal">
+                      <div className="icon icon-rose">
+                        <i className="material-icons">library_books</i>
+                      </div>
+                      <div className="description">
+                        <h4 className="info-title">2. Instilling Knowledge</h4>
+                        <p>
+                          i3 provides learning and empowerment for the Muslim Youth
+                          for them to be confident, productive and aware individuals who can address
+                          racism and Islamophobia in society.
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="col-md-4">
+                    <div className="info info-horizontal">
+                      <div className="icon icon-rose">
+                        <i className="material-icons">personal_video</i>
+                      </div>
+                      <div className="description">
+                        <h4 className="info-title">3. Wholistic Approach</h4>
+                        <p>
+                          i3 will maintain a center wherein individuals’ spiritual, social, cultural and
+                          religious identity is built.
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="features-3">
+                <div className="row">
+                  <div className="col-md-6">
+                    <br />
+                    <br />
+                    <div>
+                      <img style={{width: '90%'}} src="/myAssets/img/knowledge.png" />
+                    </div>
+                  </div>
+                  <div className="col-md-6">
+                    <h2 className="title">Our Track Record</h2>
+                    <div className="info info-horizontal">
+                      <div className="icon icon-primary">
+                        <i className="material-icons">arrow_forward</i>
+                      </div>
+                      <div className="description">
+                        <h4 className="info-title">Our Impact</h4>
+                        <p>
+                          As a grassroots organization, i3 has provided educational programs, social
+                          events, workshops and seminars that have had an impact on hundreds of Muslim
+                          youth over the past four years. Many of these events included vital modules that
+                          addressed various forms of racism faced by other minority groups that we’ve
+                          worked with; our attendees and students found them incredibly beneficial.
+                        </p>
+                      </div>
+                    </div>
+                    <div className="info info-horizontal">
+                      <div className="icon icon-primary">
+                        <i className="material-icons">money_off</i>
+                      </div>
+                      <div className="description">
+                        <h4 className="info-title">An unbeatable offer!</h4>
+                        <p>
+                          Our educational style, even though deep and intensive given the
+                          nature of the material taught, can easily be adapted into the busy
+                          lifestyle of Canadian youth. More importantly,
+                          all our programming is free of charge to ease the process of
+                          seeking Islamic knowledge.
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Layout>
 )
 
 export default History
