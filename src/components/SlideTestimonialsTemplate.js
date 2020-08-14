29be24b14d551d45369f04e3ef9fed4598403de7
Burhan Ogutalan <abogutalan@Burhans-MacBook-Pro.local>
2020-08-13 21:31:34 -0400

    slide test

--- src/components/SlideTestimonialsTemplate.js
+++ src/components/SlideTestimonialsTemplate.js
@@ -2,44 +2,34 @@ import React from 'react';
 import SlideTestimonial from './SlideTestimonial';
 // import PropTypes from 'prop-types'
 
-const SlideTestimonialsTemplate = ({ slidetestimonials, activeIndex, gotoPrev, gotoNext }) => {
+const SlideTestimonialsTemplate = ({ slidetestimonials, activeIndex }) => {
 
     console.log("slidetestimonials")
     console.log(slidetestimonials)
 
-    return(
+    return (
 
-    <>
-        <div className="testimonials-2 section-dark">
-            <div className="container">
-                <div className="row">
-                    <div className="col-md-12">
-                        <div id="carouselExampleIndicatorss" className="carousel slide" data-ride="carousel">
-                            <div className="carousel-inner">
-                                {/* slidetestimonials begin here */}
+        <>
+            <div className="testimonials-2 section-dark">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-12">
+                            <div id="carouselExampleIndicatorss" className="carousel slide" data-ride="carousel">
+                                <div className="carousel-inner">
+                                    {/* slidetestimonials begin here */}
 
-                                {slidetestimonials.map((slide, idx) => <SlideTestimonial {...slide} isActive={idx === activeIndex} />)}
+                                    {slidetestimonials.map((slide, idx) => <SlideTestimonial {...slide} isActive={idx === activeIndex} />)}
 
-                                <a className="carousel-control-prev" role="button" href="/#"
-                                    onClick={gotoPrev} >
-                                    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-                                    <span className="sr-only"> Previous </span>
-                                </a>
 
-                                <a className="carousel-control-next" role="button" href="/#"
-                                    onClick={gotoNext} >
-                                    <span className="carousel-control-next-icon" aria-hidden="true"></span>
-                                    <span className="sr-only">Next</span>
-                                </a>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
-    </>
+        </>
 
-)
+    )
 }
 
 
