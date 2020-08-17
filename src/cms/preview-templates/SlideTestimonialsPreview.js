662aa5de8fc4888e2f67a788be738d798c022a5e
Burhan Ogutalan <abogutalan@Burhans-MacBook-Pro.local>
2020-08-17 12:59:14 -0400

    new slide added

--- src/cms/preview-templates/SlideTestimonialsPreview.js
+++ src/cms/preview-templates/SlideTestimonialsPreview.js
@@ -1,26 +1,27 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import SlideTestimonial from '../../components/SlideTestimonial'
+// import SlideTestimonial from '../../components/SlideTestimonial'
 // import SlideTestimonialsTemplate from '../../components/SlideTestimonialsTemplate'
 
 const SlideTestimonialsPreview = ({ entry, getAsset }) => {
 
-  // const slidetestimonials = [{
-  //   author: entry.getIn(['data', 'author']),
-  //   imgURL: getAsset(entry.getIn(['data', 'imgURL'])),
-  //   position: entry.getIn(['data', 'position']),
-  //   quote: entry.getIn(['data', 'quote']),
-  // }]
-  // console.log('slidetestimonials in Preview')
-  // console.log(slidetestimonials)
+  const slidetestimonials = [{
+    author: entry.getIn(['data', 'author']),
+    imgSlider: getAsset(entry.getIn(['data', 'imgSlider'])).path,
+    position: entry.getIn(['data', 'position']),
+    quote: entry.getIn(['data', 'quote']),
+  }]
+  console.log('slidetestimonials in Preview')
+  console.log(slidetestimonials)
   return (
     // <SlideTestimonialsTemplate slidetestimonials={slidetestimonials}  />
-    <SlideTestimonial
-      author={entry.getIn(['data', 'author'])}
-      imgURL={getAsset(entry.getIn(['data', 'imgURL']))}
-      position={entry.getIn(['data', 'position'])}
-      quote={entry.getIn(['data', 'quote'])}
-    />
+    // <SlideTestimonial
+    //   author={entry.getIn(['data', 'author'])}
+    //   imgSlider={getAsset(entry.getIn(['data', 'imgSlider'])).path}
+    //   position={entry.getIn(['data', 'position'])}
+    //   quote={entry.getIn(['data', 'quote'])}
+    // />
+    <></>
   )
 }
 
--- src/pages/about/testimonials/sliders/marjaan-testimonials.md
+++ src/pages/about/testimonials/sliders/marjaan-testimonials.md
@@ -1,6 +1,6 @@
 ---
 templateKey: carouselTemplate
-imgSlider: /myAssets/img/testimonials/salman.png
+imgSlider: /myAssets/img/testimonials/marjaan.jpg
 quote: >-
   "After joining i3, I realized it wasn't just about the theoretical knowledge.
   It was about putting the ideas into motion. Now I'm a Project Manager at i3.
@@ -8,6 +8,6 @@ quote: >-
   students are encouraged to participate or lead such projects! 
 
   Joining the i3 program - I highly recommend it. It's a life-changing experience."
-author: Salman ibn As Sakhr
+author: Marjaan
 position: I3 PROJECT MANAGER
 ---
